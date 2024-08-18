863ae0dcaca698f07fa20a19c1398ee7c9ebbc13
Sunny Shakya <51647092+Sunny-Shakya-ss7@users.noreply.github.com>
2024-08-18 19:29:56 +0545

    flashcard generation flow complete

--- app/flashcard/page.js
+++ app/flashcard/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { db } from "@/firebase";
+import getRandomColor from "@/utils/getRandomCardColor";
 import { useUser } from "@clerk/nextjs";
 import {
   Box,
@@ -11,7 +12,7 @@ import {
   Grid,
   Typography,
 } from "@mui/material";
-import { collection, doc, getDocs } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -19,6 +20,7 @@ export default function Flashcard() {
   const { isLoaded, isSignedIn, user } = useUser();
   const [flashcards, setFlashcards] = useState([]);
   const [flipped, setFlipped] = useState({});
+  const [cardColors, setCardColors] = useState({});
 
   const searchParams = useSearchParams();
   const search = searchParams.get("id");
@@ -27,23 +29,25 @@ export default function Flashcard() {
     async function getFlashcard() {
       if (!search || !user) return;
 
-      const colRef = collection(doc(collection(db, "users"), user.id), search);
-      const docs = await getDocs(colRef);
+      const colRef = doc(db, "users", user.id, "flashcardSets", search);
+
+      const docs = await getDoc(colRef);
       const flashcards = [];
 
-      docs.forEach((doc) => {
-        flashcards.push({ id: doc.id, ...doc.data() });
-      });
+      if (docs.exists()) {
+        const flashcardSet = docs.data();
+        const flashcardsArray = flashcardSet.flashcards || [];
+        setFlashcards(flashcardsArray);
 
-      setFlashcards(flashcards);
-
-      const testColRef = collection(db, "users", user.id, "flashcardSets");
-
-      const testSnapshot = await getDocs(testColRef);
-
-      testSnapshot.forEach((doc) => {
-        console.log("Test Flashcard:", doc.data());
-      });
+        // Generate a color for each flashcard and store it in state
+        const colors = {};
+        flashcardsArray.forEach((_, index) => {
+          colors[index] = getRandomColor();
+        });
+        setCardColors(colors);
+      } else {
+        console.log("No such document!");
+      }
     }
     getFlashcard();
   }, [search, user]);
@@ -57,61 +61,79 @@ export default function Flashcard() {
 
   return (
     <Container maxWidth="md">
-      <Grid container spacing={3} sx={{ mt: 4 }}>
-        {flashcards.map((flashcard) => (
-          <Grid item xs={12} sm={6} md={4} key={flashcard.id}>
-            <Card>
-              <CardActionArea onClick={() => handleCardClick(flashcard.id)}>
-                <CardContent>
-                  <Box
-                    sx={{
-                      perspective: "1000px",
-                      "& > div": {
-                        transition: "transform 0.6s",
-                        transformStyle: "preserve-3d",
-                        position: "relative",
-                        width: "100%",
-                        height: "200px",
-                        boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
-                        transform: flipped[index]
-                          ? "rotateY(180deg)"
-                          : "rotateY(0deg)",
-                      },
-                      "& > div > div": {
-                        position: "absolute",
-                        width: "100%",
-                        height: "100%",
-                        backfaceVisibility: "hidden",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        padding: 2,
-                        boxSizing: "border-box",
-                      },
-                      "& > div > div:nth-of-type(2)": {
-                        transform: "rotateY(180deg)",
-                      },
-                    }}
-                  >
-                    <div>
-                      <div>
-                        <Typography variant="h5" component="div">
-                          {flashcard.front}
-                        </Typography>
-                      </div>
-                      <div>
-                        <Typography variant="h5" component="div">
-                          {flashcard.back}
-                        </Typography>
-                      </div>
-                    </div>
-                  </Box>
-                </CardContent>
-              </CardActionArea>
-            </Card>
+      <Box sx={{ my: 3 }}>
+        <Typography variant="h2" textAlign="center">
+          {search}
+        </Typography>
+      </Box>
+      {flashcards.length > 0 && (
+        <Box sx={{ mt: 4 }}>
+          <Grid container spacing={3}>
+            {flashcards.map((flashcard, index) => {
+              const randomColor = cardColors[index];
+
+              return (
+                <Grid item xs={12} sm={6} md={4} key={index}>
+                  <Card>
+                    <CardActionArea
+                      onClick={() => {
+                        handleCardClick(index);
+                      }}
+                    >
+                      <CardContent>
+                        <Box
+                          sx={{
+                            perspective: "1000px",
+                            "& > div": {
+                              transition: "transform 0.6s",
+                              transformStyle: "preserve-3d",
+                              position: "relative",
+                              width: "100%",
+                              height: "200px",
+                              boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+                              transform: flipped[index]
+                                ? "rotateY(180deg)"
+                                : "rotateY(0deg)",
+                            },
+                            "& > div > div": {
+                              position: "absolute",
+                              width: "100%",
+                              height: "100%",
+                              backfaceVisibility: "hidden",
+                              display: "flex",
+                              justifyContent: "center",
+                              alignItems: "center",
+                              padding: 2,
+                              boxSizing: "border-box",
+                              backgroundColor: randomColor,
+                            },
+                            "& > div > div:nth-of-type(2)": {
+                              transform: "rotateY(180deg)",
+                            },
+                          }}
+                        >
+                          <div>
+                            <div>
+                              <Typography variant="h5" component="div">
+                                {flashcard.front}
+                              </Typography>
+                            </div>
+                            <div>
+                              <Typography variant="h5" component="div">
+                                {flashcard.back}
+                              </Typography>
+                            </div>
+                          </div>
+                        </Box>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </Grid>
+              );
+            })}
           </Grid>
-        ))}
-      </Grid>
+        </Box>
+      )}
     </Container>
   );
 }
--- app/flashcards/page.js
+++ app/flashcards/page.js
@@ -1,8 +1,10 @@
 "use client";
 
 import { db } from "@/firebase";
+import getRandomColor from "@/utils/getRandomCardColor";
 import { useUser } from "@clerk/nextjs";
 import {
+  Box,
   Card,
   CardActionArea,
   CardContent,
@@ -42,10 +44,15 @@ export default function Flashcards() {
 
   return (
     <Container maxWidth="md">
+      <Box sx={{ my: 3 }}>
+        <Typography variant="h2" textAlign="center">
+          Flashcards
+        </Typography>
+      </Box>
       <Grid container spacing={3} sx={{ mt: 4 }}>
         {flashcards.map((flashcard, index) => (
           <Grid item xs={12} sm={6} md={4} key={index}>
-            <Card>
+            <Card sx={{ backgroundColor: getRandomColor }}>
               <CardActionArea onClick={() => handleCardClick(flashcard.name)}>
                 <CardContent>
                   <Typography variant="h5" component="div">
